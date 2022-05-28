519074f78eba6dc4a0ce48c9efcdf2ac73163a12
hftamayo <hftamayo@gmail.com>
2022-05-28 14:30:21 -0600

    updating routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   get '/plines', to: redirect('/productlines')
+  get '/provider', to: redirect('/prodproviders')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
